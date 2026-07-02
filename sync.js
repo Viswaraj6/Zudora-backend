e16fe91cbc00b59ac9157550da10c858e9f9292d
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-03 01:35:19 +0530

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,26 +1,48 @@
 const axios = require("axios");
 const { getAccessToken } = require("./zoho");
+const mongoose = require("mongoose");
 
-async function syncItems(){
+const Product = mongoose.model("Product");
+
+async function syncItems() {
 
     const token = await getAccessToken();
 
     const res = await axios.get(
         "https://www.zohoapis.in/inventory/v1/items",
         {
-            params:{
-                organization_id:process.env.ZOHO_ORGANIZATION_ID
+            params: {
+                organization_id: process.env.ZOHO_ORGANIZATION_ID
             },
-            headers:{
-                Authorization:`Zoho-oauthtoken ${token}`
+            headers: {
+                Authorization: `Zoho-oauthtoken ${token}`
             }
         }
     );
 
-    console.log(res.data.items);
+    for (const item of res.data.items) {
+
+        await Product.findOneAndUpdate(
+            { styleNo: item.sku },
+
+            {
+                name: item.name,
+                styleNo: item.sku,
+                price: item.rate || 0,
+                stock: item.stock_on_hand || 0,
+                category: "",
+                color: "",
+                images: [],
+                primaryImage: ""
+            },
+
+            { upsert: true, new: true }
+        );
+
+    }
+
+    console.log("Sync Completed:", res.data.items.length);
 
 }
 
-module.exports={
-    syncItems
-};
+module.exports = { syncItems };
