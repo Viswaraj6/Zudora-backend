a026d05d504b5177c892a0dce4e2aceb185f4813
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-05 00:36:36 +0530

    Update sync.js

--- sync.js
+++ sync.js
@@ -50,7 +50,8 @@ console.log("ITEM:", item.item_id, item.sku);
                 }
             }
         );
-
+console.log("LOCATION RESPONSE:");
+console.log(JSON.stringify(locationRes.data, null, 2));
         locations = locationRes.data.item_location_details.locations;
 
       console.log("SUCCESS:", item.item_id, item.sku);
