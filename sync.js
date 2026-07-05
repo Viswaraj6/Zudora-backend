5fe91ee062f256980600a7bd16bfc5c7ae0efe8d
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-05 10:12:18 +0530

    Update sync.js

--- sync.js
+++ sync.js
@@ -116,10 +116,7 @@ if (
 
 });
         }
-        product.locationStock = locations.map(loc => ({
-    location: loc.location_name,
-    stock: Number(loc.location_available_for_sale_stock)
-}));
+       
 
         const index = product.sizeStock.findIndex(
             s => s.size === size
